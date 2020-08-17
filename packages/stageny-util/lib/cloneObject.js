737c7a65686214aade9c4118d1c27c2c0c347bd5
Severin Klaus <sk@hinderlingvolkart.com>
2020-08-17 11:33:39 +0200

    Clone object could not handle circular references. Now it can.

--- packages/stageny-util/lib/cloneObject.js
+++ packages/stageny-util/lib/cloneObject.js
@@ -1,23 +1,36 @@
 module.exports = function cloneObject(data, valueTransformer = null) {
-	if (!(typeof data === "object") || !data) return data
-	const result = data instanceof Array ? [] : {}
-	Object.keys(data).forEach((key) => {
-		let val = data[key]
-		val = valueTransformer ? valueTransformer(val, key) : val
-		if (
-			valueTransformer &&
-			val &&
-			typeof val === "object" &&
-			val.key &&
-			val.value
-		) {
-			key = val.key
-			val = val.value
-		}
-		if (val && typeof val === "object") {
-			val = cloneObject(val, valueTransformer)
-		}
-		result[key] = val
-	})
-	return result
+	return deepClone(data, valueTransformer)
+}
+
+function deepClone(obj, valueTransformer = null, hash = new WeakMap()) {
+	// Do not try to clone primitives or functions
+	if (typeof obj !== "object" || obj === null || obj instanceof Function)
+		return obj
+	if (hash.has(obj)) return hash.get(obj) // Cyclic reference
+	const result = new obj.constructor()
+	// Register in hash
+	hash.set(obj, result)
+	// Clone and assign enumerable own properties recursively
+	return Object.assign(
+		result,
+		...Object.keys(obj).map((key) => {
+			let val = valueTransformer
+				? valueTransformer(obj[key], key)
+				: obj[key]
+
+			// valueTransformer can return { key: String, value: * }
+			if (
+				valueTransformer &&
+				val &&
+				typeof val === "object" &&
+				val.key &&
+				val.value
+			) {
+				key = val.key
+				val = val.value
+			}
+
+			return { [key]: deepClone(val, valueTransformer, hash) }
+		})
+	)
 }
